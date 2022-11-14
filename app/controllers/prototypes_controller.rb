df49888c5c4071f53aa467a1952aff770ddb66c6
yyy333yyy <h0abzu.emyaa3483@gmail.com>
2022-11-14 20:41:09 +0900

    未ログイン時は編集できずログイン画面へ

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
 
-  before_action :authenticate_user!, except: [:index, :show, :edit]
+  before_action :authenticate_user!, except: [:index, :show]
 
 
   def index
